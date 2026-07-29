51a136cf6dd2a04f5eb725f4a3bdccc27198b353
haorwen <haorwen@qq.com>
2026-07-29 13:11:37 +0800

    Bump record to ^6.2.1 to fix Windows build
    record_linux 0.7.2 didn't implement the hasPermission(request:)
    signature added by a newer record_platform_interface, which pub
    resolved anyway under the old ^5.2.0 constraint. Bumping to 6.2.1
    (the highest version supporting Dart 3.6.0) pulls in matching
    record_linux/record_platform_interface versions.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,7 +16,7 @@ import irondash_engine_context
 import just_audio
 import package_info_plus
 import path_provider_foundation
-import record_darwin
+import record_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import super_native_extensions
@@ -36,7 +36,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  RecordPlugin.register(with: registry.registrar(forPlugin: "RecordPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   SuperNativeExtensionsPlugin.register(with: registry.registrar(forPlugin: "SuperNativeExtensionsPlugin"))
